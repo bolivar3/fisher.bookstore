3c29790a50d7210e57bb3f07ecd3afe16ba2ba2e
Sergio Bolivar <bolivar.3@osu.edu>
2019-02-20 12:58:54 -0500

    created project shell

--- API/Program.cs
+++ API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Fisher.Bookstore
+namespace fisher.bookstore.api
 {
     public class Program
     {
